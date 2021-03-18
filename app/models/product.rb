e54b78591869ac5468935441daa93cde0e76e98c
Chayapol-c <chayapol.cha@ku.th>
2021-03-18 17:31:55 +0700

    add files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     validates :title, presence: true
     validates :desc, presence: true, length: {minimum:3}
+    validates :stock, numericality: {greater_than_or_equal_to: 0}
 end
