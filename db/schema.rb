763f9c76ae329a2a1f75a939699f7e20539985ad
Chayapol-c <chayapol.cha@ku.th>
2021-04-10 00:33:10 +0700

    add files

--- db/schema.rb
+++ db/schema.rb
@@ -30,15 +30,6 @@ ActiveRecord::Schema.define(version: 2021_04_01_075126) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "product_catagories", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_product_catagories_on_category_id"
-    t.index ["product_id"], name: "index_product_catagories_on_product_id"
-  end
-
   create_table "product_categories", force: :cascade do |t|
     t.integer "product_id", null: false
     t.integer "category_id", null: false
@@ -57,8 +48,6 @@ ActiveRecord::Schema.define(version: 2021_04_01_075126) do
     t.integer "status", default: 0, null: false
   end
 
-  add_foreign_key "product_catagories", "categories"
-  add_foreign_key "product_catagories", "products"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
 end
