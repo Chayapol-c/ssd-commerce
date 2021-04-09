003e45206bd181023249c3e9f8f34a81981e63a8
Chayapol-c <chayapol.cha@ku.th>
2021-04-10 00:15:24 +0700

    add files

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.0'
 
-
+# For file storage
+gem 'aws-sdk-s3'
 # For pagination
 gem 'kaminari'
 # For authentication
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,68 +1,72 @@
-class ProductsController < ApplicationController
-    before_action :authenticate_admin!, except: [:index, :show]
-
-    def index
-        @products = Product.all
-
-        respond_to do |format|
-            format.html {  }
-            format.csv { send_data generate_csv(Product.all), file_name: 'products.csv' }
+module Admins
+    class ProductsController < ApplicationController
+        before_action :authenticate_admin!, except: [:index, :show]
+    
+        def index
+            @products = Product.all
+    
+            respond_to do |format|
+                format.html {  }
+                format.csv { send_data generate_csv(Product.all), file_name: 'products.csv' }
+            end
         end
-    end
-
-    def show
-        @product = Product.find(params[:id])
-    end
-
-    def new
-        @product = Product.new
-        @all_categories = Category.all
-        @product_category = @product.product_categories.build
-    end
-
-    def edit
-        @product = Product.find(params[:id])
-    end
-
-    def create
-        @product = Product.create(product_params)
-
-        if @product.invalid?
-            flash[:error] = @product.errors.full_messages
+    
+        def show
+            @product = Product.find(params[:id])
         end
-
-        redirect_to action: :index
-    end
-
-    def update
-        @product = Product.find(params[:id])
-        @product.update(product_params)
-        redirect_to action: :index
-    end
-
-    def destroy
-        @product = Product.find(params[:id])
-        @product.destroy
-        redirect_to action: :index
-    end
-
-    def csv_upload
-        data = params[:csv_file].read.split("\n")
-        data.each do |line|
-            attribute = line.split(",").map(&:strip)
-            Product.create title: attribute[0], desc: attribute[1], stock: attribute[2]
-            puts data
+    
+        def new
+            @product = Product.new
+            @all_categories = Category.all
+            @product_category = @product.product_categories.build
         end
-        redirect_to action: :index
+    
+        def edit
+            @product = Product.find(params[:id])
+        end
+    
+        def create
+            @product = Product.create(product_params)
+    
+            if @product.invalid?
+                flash[:error] = @product.errors.full_messages
+            end
+    
+            redirect_to action: :index
+        end
+    
+        def update
+            @product = Product.find(params[:id])
+            @product.update(product_params)
+            redirect_to action: :index
+        end
+    
+        def destroy
+            @product = Product.find(params[:id])
+            @product.destroy
+            redirect_to action: :index
+        end
+    
+        def csv_upload
+            data = params[:csv_file].read.split("\n")
+            data.each do |line|
+                attribute = line.split(",").map(&:strip)
+                Product.create title: attribute[0], desc: attribute[1], stock: attribute[2]
+                puts data
+            end
+            redirect_to action: :index
+        end
+    
+    
+        private
+        def generate_csv(products)
+            products.map { |p| [p.title, p.desc, p.stock].join(',') }.join("\n")
+        end
+    
+        def product_params
+            params.require(:product).permit(:title, :desc, :status, :stock, category_ids: [])
+            # params.require(:product).permit(:title, :desc, :status, :stock, :primary_image, category_ids: [])
+          end
     end
-
-
-    private
-    def generate_csv(products)
-        products.map { |p| [p.title, p.desc, p.stock].join(',') }.join("\n")
-    end
-
-    def product_params
-        params.require(:product).permit(:title, :desc, :status, :stock, category_ids: [])
-      end
+    
 end
