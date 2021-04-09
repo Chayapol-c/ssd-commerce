09b6fd467d1d0317627da234cf68a6b79854c800
Chayapol-c <chayapol.cha@ku.th>
2021-04-10 00:37:16 +0700

    add file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
     
-    enum status: { draft: 0, published: 1, archived: 2  }
+    enum status: [:draft, :published, :archived]
+
+    # has_one_attached :primary_image
+
 
     has_many :product_categories, dependent: :destroy
     has_many :categories, through: :product_categories
