39937b79b8ea6b27e3b054a34688c1a3a03c1804
Chayapol-c <chayapol.cha@ku.th>
2021-04-10 01:02:11 +0700

    add configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "products#index"
-  get "/products", to: "products#index"
+  # root "products#index"
+  # get "/products", to: "products#index"
   get "/categories", to: "categories#index"
-
-  namespace :products do
-    post 'csv_upload'
+  
+  root "admins/products#index"
+  
+  namespace :admins do 
+    resources :products
+    
+    namespace :products do
+      post 'csv_upload'
+    end
   end
-  resources :products
+
   resources :categories
 end
